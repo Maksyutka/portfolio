7bca9693b9a71d0270c5085d2e8d60944b022261
maximshevchenko <maksyutka4579@gmail.com>
2022-03-20 01:18:50 +0200

    scrollbar

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,7 +1,22 @@
+$(".my-popup").mouseup(function (e) {
+	// событие клика по веб-документу
+	var div = $(".my-popup__inner"); // тут указываем ID элемента
+	if (
+		!div.is(e.target) && // если клик был не по нашему блоку
+		div.has(e.target).length === 0
+	) {
+		// и не по его дочерним элементам
+		$(".my-popup").fadeOut(); // скрываем его
+		$("body").removeClass("overflowed");
+	}
+});
+
 $(".main__info button").click(function () {
 	$(".my-popup").fadeToggle();
+	$("body").toggleClass("overflowed");
 });
 
 $(".close").click(function () {
 	$(".my-popup").fadeToggle();
+	$("body").toggleClass("overflowed");
 });
